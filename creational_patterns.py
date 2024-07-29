4d7e210b7fe08e54b50d2e7e5c62852becfb8bad
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-07-28 18:13:15 -0700

    update with factory pattern

--- creational_patterns.py
+++ creational_patterns.py
@@ -1,4 +1,31 @@
 """ 
 The factory pattern wraps a constructor for different types of objects and returns instances of the objects via a simple API. It makes it easy to create different objects by exposing a simple API that return the specified object type.
 
+"Define an interface for creating an object, but let subclasses decide which class to instantiate. The Factory method lets a class defer instantiation it uses to subclasses.
+
 """
+
+class Burger:
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
+
+    def print(self):
+        print(self.ingredients)
+
+class BurgerFactory:
+    def createCheeseBurger(self):
+        ingredients = ["bun", "cheese", "beef-patty"]
+        return Burger(ingredients)
+    
+    def createDeluxeCheeseBurger(self):
+        ingredients = ["bun", "tomatoe", "lettuce", "cheese", "beef-patty"]
+        return Burger(ingredients)
+
+    def createVeganBurger(self):
+        ingredients = ["bun", "special-sauce", "veggie-patty"]
+        return Burger(ingredients)
+
+burgerFactory = BurgerFactory()
+burgerFactory.createCheeseBurger().print()
+burgerFactory.createDeluxeCheeseBurger().print()
+burgerFactory.createVeganBurger().print()
